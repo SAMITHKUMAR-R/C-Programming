ce82583561c47086ea20eac8c515d4db1ae19eaa
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-22 19:32:22 +0530

    Update Obs.2(A).c

--- Obs.2(A).c
+++ Obs.2(A).c
@@ -3,20 +3,22 @@
 int main ()
 {
     char ch;
+    
     printf("Enter the Character : ");
     scanf("%c",&ch);
+    
     ch=tolower(ch);
-    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
-    {
+    
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
         printf("%c is a vowel \n",ch);
     }
-    else if ((ch>='a')&&(ch<='z'))
-    {
+    else if ((ch>='a')&&(ch<='z') {
         printf("%c is a consonant \n",ch);
     }
-    else
-    {
+    else{
         printf("Invalid Input!");
     }
+    
     return 0;
 }
+
