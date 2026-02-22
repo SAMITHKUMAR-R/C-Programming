71d2e3c47dbfc82845c20726f2a77439fce42754
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-22 19:31:11 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -5,15 +5,20 @@ int main()
     printf("Enter Number : \n");
     scanf("%d",&n);
     temp=n;
+    
     while(n>0)
     {
         r=n%10;
         sum=(sum*10)+r;
         n=n/10;
     }
+    
     if(temp=sum)
     printf("Palindrome Number");
+        
     else
     printf("Not Palindrome Number");
+    
     return 0;
 }
+
