7bb4455ea1ab752ee115645ae450c3a02ff27144
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-22 19:30:43 +0530

    Update Obs.1(A).c

--- Obs.1(A).c
+++ Obs.1(A).c
@@ -2,13 +2,18 @@
 int main ()
 {
     float radius , circumference , diameter,area;
+
     printf("Enter the radius of the circle :");
     scanf("%f",&radius);
+    
     diameter=2*radius;
     circumference=2*3.14*radius;
     area=3.14*radius*radius;
+    
     printf("Circumference Of Circle :%.2f units\n",circumference);
     printf("Diameter Of Circle :%.2f units\n",diameter);
     printf("Area Of Circle :%.2f sq.units\n",area);
+    
     return 0;
 }
+
