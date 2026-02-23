ccfefdaf1dd2b61b92ce7a71f599fe3aec0b8a0f
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-23 23:54:34 +0530

    Update sum.matrix.c

--- sum.matrix.c
+++ sum.matrix.c
@@ -9,19 +9,24 @@ int main()
         printf("Enter a%d%d:",i+1,j+1);
         scanf("%f",&a[i][j]);
     }
+    
     printf("Enter elements of 2nd matrix\n");
+    
     for(int i=0;i<2;++i)
     for(int j=0;j<2;++j)
     {
         printf("Enter b%d%d:",i+1,j+1);
         scanf("%f",&b[i][j]);
     }
+    
     for(int i=0;i<2;++i)
     for(int j=0;j<2;++j)
     {
         result[i][j]=a[i][j]+b[i][j];
     }
+    
     printf("\nsum of matrix:\n");
+    
     for(int i=0;i<2;++i)
     for(int j=0;j<2;++j)
     {
@@ -29,8 +34,10 @@ int main()
         if(j==1)
         printf("\n");
     }
+    
     return 0;
 }
+
 
 
 
