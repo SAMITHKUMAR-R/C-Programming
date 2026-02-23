0a2b104608ac868b92d1eab992da55920d051dc8
SAMITHKUMAR R <sr.samithkumar@gmail.com>
2026-02-23 23:52:01 +0530

    Update vowcons.c

--- vowcons.c
+++ vowcons.c
@@ -3,14 +3,13 @@ void main ()
 {
     char a;
     scanf("%c",&a);
-    if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
-       {
+    if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||a=='a'||a=='e'||a=='i'||a=='o'||a=='u') {
            printf("Vowel");
-       }
-    else
-    {
+    }
+    else {
         printf("Consonance");
     }
 }
+
 
 
